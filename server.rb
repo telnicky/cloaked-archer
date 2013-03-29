f8ec6f192c20a1a8b145a71f8af42f0c644f7923
elnicky <travis.elnicky@gmail.com>
2013-03-28 21:22:33 -0600

    web socketsssssss

--- server.rb
+++ server.rb
@@ -1,7 +1,35 @@
 require 'em-websocket'
+require 'pry'
+#
+# HTTP/1.1 101 Switching Protocols
+# Upgrade: websocket
+# Connection: Upgrade
+# Sec-WebSocket-Accept: HSmrc0sMlYUkAGmm5OPpG2HaGWk=
+# Sec-WebSocket-Protocol: chat\
+sockets = []
 
-EventMachine::WebSocket.start(:host => "0.0.0.0", :port => 8080) do |ws|
-  ws.onopen    { ws.send "Hello Client!"}
-  ws.onmessage { |msg| ws.send "Pong: #{msg}" }
-  ws.onclose   { puts "WebSocket closed" }
-end
+EM.run {
+  EM::WebSocket.run(:host => "127.0.0.1", :port => 8080) do |ws|
+
+sockets << ws
+
+    ws.onopen { |handshake|
+binding.pry
+      puts "WebSocket connection open"
+      # Access properties on the EM::WebSocket::Handshake object, e.g.
+      # path, query_string, origin, headers
+
+      # Publish message to the client
+      ws.send "Hello Client, you connected to #{handshake.path}"
+    }
+
+    ws.onclose { puts "Connection closed" }
+
+    ws.onmessage { |msg|
+      puts "Recieved message: #{msg}"
+      sockets.each do |s|
+        s.send "Ponggggg: #{msg}"
+      end
+    }
+  end
+}
